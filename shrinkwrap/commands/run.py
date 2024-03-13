anthony-linaro/shrinkwrap

commit c99dc74b9fb7667e3b54e1bc44bf51acb3898930

run: Overhaul user input handling.
User input handling is overhauled so that it is now possible to use bash
within the FVP naturally. No more line buffering and 2x echoing. All key
combinations are passed through unmodified (including tabs, '^C', etc)
to allow tab completion and interrupting processes within running within
the FVP.

This is achieved by setting the input side of the user's terminal into
(mostly) raw mode and passing all input to a pty which is connected to
telnet, which is now used as the uart client for stdinout uart
terminals.

As a side-effect of passing '^C' through to the FVP, it can no longer be
used to quit shrinkwrap. Instead the '^]' key binding is used to quit
shrinkwrap. This is the only key combination not passed through.

Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

shrinkwrap/commands/run.py
@@ -93,10 +93,30 @@ def dispatch(args):
 	terminals = dict(sorted(terminals.items()))
 	for t in terminals.values():
 		t['port'] = None
+		t['strip'] = False
 		if len(t['friendly']) > name_field:
 			name_field = min(len(t['friendly']), max_name_field)
 
 	log = logger.Logger(name_field, not args.no_color)
+
+	def _strip_telnet_header(pm, proc, data):
+		"""
+		For any stdinout terminals (which use telnet), strip the first
+		few lines of output, which is output by telnet. This is
+		confusing for users since telnet is an implementation detail.
+		"""
+		match = "Escape character is '^]'.\n"
+		pdata = proc.data
+
+		for line in data.splitlines(keepends=True):
+			if len(pdata) >= 2 and terminals[pdata[1]]['strip']:
+				if line == match:
+					terminals[pdata[1]]['strip'] = False
+					if all([not t['strip'] \
+						for t in terminals.values()]):
+						pm.set_handler(log.log)
+			else:
+				log.log(pm, proc, line)
 
 	def _find_term_ports(pm, proc, data):
 		"""
@@ -127,18 +147,26 @@ def dispatch(args):
 		# and change the handler so we never get called again.
 		if found_all_ports:
 			wait = False
-			for t in terminals.values():
+			strip = False
+			for k, t in terminals.items():
 				name = t['friendly']
 				type = t['type']
 				port = t["port"]
 
-				if type in ['stdout', 'stdinout']:
-					interactive = type == 'stdinout'
+				if type in ['stdout']:
 					cmd = f'nc localhost {port}'
 					pm.add(process.Process(cmd,
-							interactive,
-							(log.alloc_data(name),),
+							False,
+							(log.alloc_data(name), k),
 							False))
+				if type in ['stdinout']:
+					cmd = f'telnet localhost {port}'
+					pm.add(process.Process(cmd,
+							True,
+							(log.alloc_data(name), k),
+							False))
+					t['strip'] = True
+					strip = True
 				if type in ['xterm']:
 					# Nothing to do. The FVP will start this
 					# automatically.
@@ -152,7 +180,10 @@ def dispatch(args):
 				print()
 				input("Press Enter to continue...")
 
-			pm.set_handler(log.log)
+			if strip:
+				pm.set_handler(_strip_telnet_header)
+			else:
+				pm.set_handler(log.log)
 
 	# Run under a runtime environment, which may just run commands natively
 	# on the host or may execute commands in a container, depending on what
@@ -184,4 +215,10 @@ def dispatch(args):
 					False,
 					(log.alloc_data('fvp'),),
 					True))
+
+			print()
+			print("Press '^]' to quit shrinkwrap.")
+			print("All other keys are passed through.")
+			print()
+
 			pm.run()

shrinkwrap/utils/config.py
@@ -555,13 +555,13 @@ def resolver(config, rtvars={}, clivars={}):
 
 	terms = []
 	for param, terminal in run['terminals'].items():
-		if terminal['type'] in ['stdout', 'stdinout']:
+		if terminal['type'] in ['stdout']:
 			terms.append(f'-C {param}.start_telnet=0')
 			terms.append(f'-C {param}.mode=raw')
 		if terminal['type'] in ['xterm']:
 			terms.append(f'-C {param}.start_telnet=1')
 			terms.append(f'-C {param}.mode=telnet')
-		if terminal['type'] in ['telnet']:
+		if terminal['type'] in ['telnet', 'stdinout']:
 			terms.append(f'-C {param}.start_telnet=0')
 			terms.append(f'-C {param}.mode=telnet')
 	terms = ' '.join(terms)

shrinkwrap/utils/process.py
@@ -1,9 +1,13 @@
 import fcntl
+import io
 import os
+import pty
 import shlex
 import selectors
 import subprocess
+import sys
 import shrinkwrap.utils.runtime as runtime
+import shrinkwrap.utils.tty as tty
 
 
 class Process:
@@ -16,6 +20,8 @@ class Process:
 		self.data = data
 		self.run_to_end = run_to_end
 		self._popen = None
+		self._stdout = None
+		self._stdin = None
 
 
 class ProcessManager:
@@ -37,34 +43,27 @@ class ProcessManager:
 	def add(self, process):
 		self._procs.append(process)
 		if self._sel:
-			self._activate(process)
+			self._proc_activate(process)
 
 	def run(self):
 		try:
 			self._sel = selectors.DefaultSelector()
 			self._active = 0
 
+			self._stdin_activate()
+
 			for proc in self._procs:
-				self._activate(proc)
+				self._proc_activate(proc)
 
 			while self._active > 0:
 				for key, mask in self._sel.select():
-					proc = key.data
-					data = key.fileobj.read()
-
-					if data == '':
-						self._deactivate(proc)
-					else:
-						if self._handler:
-							self._handler(self,
-								      proc,
-								      data)
+					handler = key.data[0]
+					handler(key, mask)
 		finally:
 			for proc in self._procs:
-				try:
-					self._deactivate(proc, force=True)
-				except:
-					pass
+				self._proc_deactivate(proc, force=True)
+
+			self._stdin_deactivate()
 
 			self._sel.close()
 			self._sel = None
@@ -74,29 +73,53 @@ class ProcessManager:
 		fcntl.fcntl(fileobj, fcntl.F_SETFL, fl | os.O_NONBLOCK)
 		self._sel.register(fileobj, selectors.EVENT_READ, data)
 
-	def _activate(self, proc):
-		stdin=None if proc.interactive else subprocess.DEVNULL
+	def _proc_handle(self, key, mask):
+		proc = key.data[1]
+		data = key.fileobj.read()
 
-		proc._popen = subprocess.Popen(runtime.mkcmd(proc.args,
-							     proc.interactive),
-					       stdin=stdin,
-					       stdout=subprocess.PIPE,
-					       stderr=subprocess.STDOUT,
-					       text=True)
+		if data == '':
+			self._proc_deactivate(proc)
+		else:
+			if self._handler:
+				self._handler(self, proc, data)
 
-		self._register(proc._popen.stdout, proc)
+	def _proc_activate(self, proc):
+		cmd = runtime.mkcmd(proc.args, proc.interactive)
+
+		if proc.interactive:
+			master, slave = pty.openpty()
+
+			proc._popen = subprocess.Popen(cmd,
+						       stdin=slave,
+						       stdout=slave,
+						       stderr=slave)
+
+			proc._stdin = io.open(master, 'wb', buffering=0)
+			proc._stdout = io.open(master, 'rb', -1, closefd=False)
+			proc._stdout = io.TextIOWrapper(proc._stdout)
+		else:
+			proc._popen = subprocess.Popen(cmd,
+						       stdin=subprocess.DEVNULL,
+						       stdout=subprocess.PIPE,
+						       stderr=subprocess.STDOUT,
+						       text=True)
+
+			proc._stdin = None
+			proc._stdout = proc._popen.stdout
+
+		self._register(proc._stdout, (self._proc_handle, proc))
 
 		if proc.run_to_end:
 			self._active += 1
 
-	def _deactivate(self, proc, force=False):
+	def _proc_deactivate(self, proc, force=False):
 		if not proc._popen:
 			return
 
 		if proc.run_to_end:
 			self._active -= 1
 
-		self._sel.unregister(proc._popen.stdout)
+		self._sel.unregister(proc._stdout)
 
 		proc._popen.kill()
 		try:
@@ -107,5 +130,49 @@ class ProcessManager:
 		retcode = None if force else proc._popen.poll()
 		proc._popen = None
 
+		if proc._stdin:
+			proc._stdin.close()
+			proc._stdin = None
+
+		if proc._stdout:
+			proc._stdout.close()
+			proc._stdout = None
+
 		if self._terminate_handler:
-			self._terminate_handler(self, proc, retcode)
+			try:
+				self._terminate_handler(self, proc, retcode)
+			except:
+				pass
+
+	def _stdin_handle(self, key, mask):
+		data = key.fileobj.read()
+		for proc in self._procs:
+			if proc._stdin:
+				proc._stdin.write(data)
+
+	def _stdin_activate(self):
+		# Replace stdin with unbuffered binary stream so we can pass
+		# input to the ptys without any modification.
+		self._stdin_orig = sys.stdin
+		sys.stdin = io.open(self._stdin_orig.fileno(),
+				    'rb',
+				    buffering=0,
+				    closefd=False)
+
+		# Set the terminal to raw input mode.
+		self._tty_orig = tty.configure(sys.stdin)
+
+		# Register stdin so we get notified when there is data.
+		self._register(sys.stdin, (self._stdin_handle,))
+
+	def _stdin_deactivate(self):
+		# Unregister for notifications.
+		self._sel.unregister(sys.stdin)
+
+		# Restore terminal mode.
+		tty.restore(sys.stdin, self._tty_orig)
+
+		# Restore stdin to being buffered text.
+		sys.stdin.close()
+		sys.stdin = self._stdin_orig
+		self._stdin_orig = None

shrinkwrap/utils/runtime.py
@@ -32,12 +32,7 @@ class Runtime:
 		self._rt.add_volume(src, dst)
 
 	def mkcmd(self, cmd, interactive=False):
-		cmd = self._rt.get_command_line(cmd, interactive, False)
-		try:
-			cmd.remove('--tty')
-		except ValueError:
-			pass
-		return cmd
+		return self._rt.get_command_line(cmd, interactive, False)
 
 	def ip_address(self):
 		"""

shrinkwrap/utils/tty.py
@@ -1,0 +1,43 @@
+from termios import *
+import copy
+
+IFLAG = 0
+OFLAG = 1
+CFLAG = 2
+LFLAG = 3
+ISPEED = 4
+OSPEED = 5
+CC = 6
+
+def configure(fd, when=TCSAFLUSH):
+	try:
+		orig = tcgetattr(fd)
+		mode = copy.deepcopy(orig)
+
+		# Don't remove/convert line feeds to new lines on input. EDK2
+		# can't handle only receiving newlines.
+		mode[IFLAG] = mode[IFLAG] & ~(INLCR | IGNCR | ICRNL)
+
+		# Disable input echo and use non-canonical input mode (no
+		# buffering).
+		mode[LFLAG] = mode[LFLAG] & ~(ECHO | ICANON)
+		mode[CC][VMIN] = 1
+		mode[CC][VTIME] = 0
+
+		# Use ^] as the interrupt (instead of ^C), and remove key
+		# bindings for quit and suspend signals. This means the usual
+		# key bindings are passed through to the child.
+		mode[CC][VINTR] = b'\x1d'
+		mode[CC][VQUIT] = b'\x00'
+		mode[CC][VSUSP] = b'\x00'
+
+		tcsetattr(fd, when, mode)
+		return orig
+
+	except error as e:
+		# Expect failure if we are not connected to a tty. Non-fatal.
+		pass
+
+def restore(fd, mode, when=TCSAFLUSH):
+	if mode:
+		tcsetattr(fd, when, mode)

commit 145ec14c9dfb5de4f49170248815fb70195968b9

run: Fix broken user ack when using telnet terminal
If any terminal type is set to 'telnet', Shrinkwrap prints the required
telnet command that the user needs to connect, and waits for the user to
acknowledge by pressing enter. It uses the standard Python input()
function for this. However, the process manager has already redirected
stdin so input() never receives any input and never returns.

Fix this by explicitly requesting the process manager deactivates stdin
around the input() call.

Fixes: c99dc74 ("run: Overhaul user input handling.")
Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

shrinkwrap/commands/run.py
@@ -199,8 +199,11 @@ def dispatch(args):
 					print(f'To start {name} terminal, run:')
 					print(f'    telnet {ip} {port}')
 			if wait:
+				# Temporarily restore sys.stdin for input().
+				pm._stdin_deactivate()
 				print()
 				input("Press Enter to continue...")
+				pm._stdin_activate()
 
 			if strip:
 				pm.set_handler(_strip_telnet_header)