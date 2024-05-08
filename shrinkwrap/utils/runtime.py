anthony-linaro/shrinkwrap

commit 1db05f5ab4e3cb5743610f7a043361c54e48d057

runtime: replace modal stack with singleton
Signed-off-by: Gareth Stockwell <gareth.stockwell@arm.com>

shrinkwrap/utils/runtime.py
@@ -8,7 +8,7 @@ import tuxmake.runtime
 import types
 
 
-_stack = []
+_instance = None
 
 
 def get_null_user_opts(self):
@@ -50,8 +50,6 @@ class Runtime:
 		else:
 			self._rt.set_user('shrinkwrap')
 			self._rt.set_group('shrinkwrap')
-
-		_stack.append(self)
 
 	def start(self):
 		for mp in self._mountpoints:
@@ -116,27 +114,26 @@ print(ip)
 		if res.returncode == 0:
 			return res.stdout.strip()
 		return '127.0.0.1'
-
-	def cleanup(self):
-		if self._rt:
-			self._rt.cleanup()
-			self._rt = None
-			s = _stack.pop()
-			assert(s == self)
-
+	
 	def __enter__(self):
+		global _instance
+		assert _instance is None
+		_instance = self
 		return self
-
+	
 	def __exit__(self, exc_type, exc_val, exc_tb):
-		self.cleanup()
+		global _instance
+		assert _instance == self
+		_instance = None
 
 
 def get():
 	"""
 	Returns the current Runtime instance.
 	"""
-	assert(len(_stack) > 0)
-	return _stack[-1]
+	global _instance
+	assert _instance is not None
+	return _instance
 
 
 def mkcmd(cmd, interactive=False):

commit c4024271ff368de857f3ae9d9ecd8df72d35504a

runtime: Ensure container gets cleaned up on shutdown
Recent refactoring to simplify the Runtime class inadvertantly removed
the cleanup code. As a result, a container would not be stopped and
removed when shrinkwrap exits but would linger, using system resources.

Fix that by reintroducing the cleanup code.

Fixes: 1db05f5 ("runtime: replace modal stack with singleton")
Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

shrinkwrap/utils/runtime.py
@@ -131,17 +131,20 @@ print(ip)
 		if res.returncode == 0:
 			return res.stdout.strip()
 		return '127.0.0.1'
-	
+
 	def __enter__(self):
 		global _instance
 		assert _instance is None
 		_instance = self
 		return self
-	
+
 	def __exit__(self, exc_type, exc_val, exc_tb):
 		global _instance
 		assert _instance == self
 		_instance = None
+		if self._rt:
+			self._rt.cleanup()
+			self._rt = None
 
 
 def get():