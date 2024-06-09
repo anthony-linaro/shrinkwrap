ab7b137fde3e462b61c9c5a368b002235a1c4026
Ryan Roberts <ryan.roberts@arm.com>
2024-06-09 09:55:22 +0100

    docker: Enhance build.sh to support kaniko
    When building images on CI, we need to use Kaniko, since docker doesn't
    work when running inside docker. So let's enhance the script to allow
    the user to specify the driver. While we are at it, also allow the user
    to specify the registry, since that will be needed for CI too, to store
    temporary images. Additionally, ensure the script runs under sh, since
    bash is not available in the kaniko container image.
    
    For some reason `uname -p` returns "unknown" on CI runners, so let's
    switch to the more standard `uname -m` instead.
    
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

--- docker/build.sh
+++ docker/build.sh
@@ -1,37 +1,95 @@
-#!/bin/bash
+#!/bin/sh
 # Copyright (c) 2022, Arm Limited.
 # SPDX-License-Identifier: MIT
 
 set -e
 
-function usage()
+usage()
 {
-    cat << EOF
+cat << EOF
 Builds and optionally publishes shrinkwrap docker images for the architecture
 of the host system. (x86_64 and aarch64 are currently supported).
 
 Usage:
-$(basename $0) <tag>
+$(basename $0) [--driver docker|kaniko] [--registry <url>] --version <tag>
 
 Where:
+  --driver determines how the image will be built:
+    docker: (default) and will use "docker build" and "docker push".
+            This is usually what you want when running locally.
+    kaniko: Uses kaniko, which does not require access to the docker daemon.
+            Useful on CI systems where running inside a container. Must be
+            running inside the kaniko container.
+
+  <url> is the registry to publish to (defaults to docker.io/shrinkwraptool).
   <tag> is something like "latest" or "v1.0.0".
 
-If <tag> is "local", the resulting image is NOT pushed to the remote repository.
+The user is responsible for having already configured the driver connection to
+the registry.
+
+If <tag> is "local" and --driver is "docker", the resulting image is NOT pushed
+to the remote repository but instead kept locally by docker.
+
 If <tag> is "none", the package cache is synced but no image is built.
 EOF
 }
 
+DRIVER="docker"
+REGISTRY="docker.io/shrinkwraptool"
+VERSION=
+ARCH=$(uname -m)
+
 # Parse command line.
-if [ "$#" -ne 1 ]; then
-    usage
-    exit 1
+while [ $# -gt 0 ]; do
+	case $1 in
+	--version)
+		VERSION="$2"
+		shift # past argument
+		shift # past value
+		;;
+	--registry)
+		REGISTRY="$2"
+		shift # past argument
+		shift # past value
+		;;
+	--driver)
+		DRIVER="$2"
+		shift # past argument
+		shift # past value
+		;;
+	-h|--help)
+		usage
+		exit 0
+		;;
+	-*|--*)
+		echo "error: unexpected named argument! ($1)"
+		usage
+		exit 1
+		;;
+	*)
+		echo "error: unexpected positional argument! ($1)"
+		usage
+		exit 1
+		;;
+	esac
+done
+
+if [ -z ${VERSION} ]; then
+	echo "error: no version provided!"
+	usage
+	exit 1
 fi
-VERSION="${1}"
-ARCH=$(uname -p)
-REGISTRY=shrinkwraptool
+
+if [ "${DRIVER}" != "docker" ] && [ "${DRIVER}" != "kaniko" ]; then
+	echo "error: invalid driver provided! (${DRIVER})"
+	usage
+	exit 1
+fi
+
+echo "Building image for ${ARCH} with driver=${DRIVER},registry=${REGISTRY},version=${VERSION}..."
 
 # Configure the arch-specific variables which are passed to the Dockerfile.
-if [ "${ARCH}" == "x86_64" ]; then
+if [ "${ARCH}" = "x86_64" ]; then
 	TCH_PKG_URL_AARCH64=https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel
 	TCH_PKG_NAME_AARCH64=arm-gnu-toolchain-13.2.rel1-x86_64-aarch64-none-elf.tar.xz
 	TCH_PATH_AARCH64=arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-elf/bin
@@ -45,8 +103,8 @@ if [ "${ARCH}" == "x86_64" ]; then
 	FVP_PKG_NAME=FVP_Base_RevC-2xAEMvA_11.24_11_Linux64.tgz
 	FVP_MODEL_DIR=Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3
 	FVP_PLUGIN_DIR=Base_RevC_AEMvA_pkg/plugins/Linux64_GCC-9.3
-# ARCH is "aarch64" on Ubuntu, or "arm" on Mac OS
-elif [ "${ARCH}" == "aarch64" ] || [ "${ARCH}" == "arm" ]; then
+# ARCH is "aarch64" on Ubuntu, or "arm64" on Mac OS
+elif [ "${ARCH}" = "aarch64" ] || [ "${ARCH}" = "arm64" ]; then
 	TCH_PKG_URL_AARCH64=https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel
 	TCH_PKG_NAME_AARCH64=arm-gnu-toolchain-13.2.rel1-aarch64-aarch64-none-elf.tar.xz
 	TCH_PATH_AARCH64=arm-gnu-toolchain-13.2.Rel1-aarch64-aarch64-none-elf/bin
@@ -64,8 +122,6 @@ else
 	echo "Host architecture ${ARCH} not supported"
 	exit 1
 fi
-
-echo "Building for version ${VERSION} for ${ARCH}..."
 
 wget_or_cache()
 {
@@ -93,44 +149,83 @@ if [ "${VERSION}" = "none" ]; then
 	exit
 fi
 
-# Build the image.
-docker build \
-	--build-arg=BASE=docker.io/library/debian:bookworm-slim \
-	--build-arg=TCH_PKG_NAME_AARCH64=${TCH_PKG_NAME_AARCH64} \
-	--build-arg=TCH_PATH_AARCH64=${TCH_PATH_AARCH64} \
-	--file=Dockerfile.slim \
-	--tag=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
-	.
-docker build \
-	--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
-	--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
-	--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
-	--build-arg=FVP_PLUGIN_DIR=${FVP_PLUGIN_DIR} \
-	--file=Dockerfile.fvp \
-	--tag=${REGISTRY}/base-slim:${VERSION}-${ARCH} \
-	.
-docker build \
-	--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
-	--build-arg=TCH_PKG_NAME_AARCH32=${TCH_PKG_NAME_AARCH32} \
-	--build-arg=TCH_PATH_AARCH32=${TCH_PATH_AARCH32} \
-	--build-arg=TCH_LLVM_PKG_NAME=${TCH_LLVM_PKG_NAME} \
-	--build-arg=TCH_LLVM_PATH=${TCH_LLVM_PATH} \
-	--file=Dockerfile.full \
-	--tag=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
-	.
-docker build \
-	--build-arg=BASE=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
-	--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
-	--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
-	--build-arg=FVP_PLUGIN_DIR=${FVP_PLUGIN_DIR} \
-	--file=Dockerfile.fvp \
-	--tag=${REGISTRY}/base-full:${VERSION}-${ARCH} \
-	.
+# Build the images.
+if [ "${DRIVER}" = "docker" ]; then
+	docker build \
+		--build-arg=BASE=docker.io/library/debian:bookworm-slim \
+		--build-arg=TCH_PKG_NAME_AARCH64=${TCH_PKG_NAME_AARCH64} \
+		--build-arg=TCH_PATH_AARCH64=${TCH_PATH_AARCH64} \
+		--file=Dockerfile.slim \
+		--tag=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
+		.
+	docker build \
+		--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
+		--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
+		--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
+		--build-arg=FVP_PLUGIN_DIR=${FVP_PLUGIN_DIR} \
+		--file=Dockerfile.fvp \
+		--tag=${REGISTRY}/base-slim:${VERSION}-${ARCH} \
+		.
+	docker build \
+		--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
+		--build-arg=TCH_PKG_NAME_AARCH32=${TCH_PKG_NAME_AARCH32} \
+		--build-arg=TCH_PATH_AARCH32=${TCH_PATH_AARCH32} \
+		--build-arg=TCH_LLVM_PKG_NAME=${TCH_LLVM_PKG_NAME} \
+		--build-arg=TCH_LLVM_PATH=${TCH_LLVM_PATH} \
+		--file=Dockerfile.full \
+		--tag=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
+		.
+	docker build \
+		--build-arg=BASE=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
+		--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
+		--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
+		--build-arg=FVP_PLUGIN_DIR=${FVP_PLUGIN_DIR} \
+		--file=Dockerfile.fvp \
+		--tag=${REGISTRY}/base-full:${VERSION}-${ARCH} \
+		.
 
-# If not a local version, publish the image.
-if [ "${VERSION}" != "local" ]; then
-	docker push ${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH}
-	docker push ${REGISTRY}/base-slim:${VERSION}-${ARCH}
-	docker push ${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH}
-	docker push ${REGISTRY}/base-full:${VERSION}-${ARCH}
+	# If not a local version, publish the image.
+	if [ "${VERSION}" != "local" ]; then
+		docker push ${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH}
+		docker push ${REGISTRY}/base-slim:${VERSION}-${ARCH}
+		docker push ${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH}
+		docker push ${REGISTRY}/base-full:${VERSION}-${ARCH}
+	fi
+elif [ "${DRIVER}" = "kaniko" ]; then
+	# Build the images.
+	/kaniko/executor \
+		--build-arg=BASE=docker.io/library/debian:bookworm-slim \
+		--build-arg=TCH_PKG_NAME_AARCH64=${TCH_PKG_NAME_AARCH64} \
+		--build-arg=TCH_PATH_AARCH64=${TCH_PATH_AARCH64} \
+		--dockerfile=Dockerfile.slim \
+		--destination=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
+		--context=.
+	/kaniko/executor \
+		--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
+		--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
+		--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
+		--build-arg=FVP_PLUGIN_DIR=${FVP_PLUGIN_DIR} \
+		--dockerfile=Dockerfile.fvp \
+		--destination=${REGISTRY}/base-slim:${VERSION}-${ARCH} \
+		--context=.
+	/kaniko/executor \
+		--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
+		--build-arg=TCH_PKG_NAME_AARCH32=${TCH_PKG_NAME_AARCH32} \
+		--build-arg=TCH_PATH_AARCH32=${TCH_PATH_AARCH32} \
+		--build-arg=TCH_LLVM_PKG_NAME=${TCH_LLVM_PKG_NAME} \
+		--build-arg=TCH_LLVM_PATH=${TCH_LLVM_PATH} \
+		--dockerfile=Dockerfile.full \
+		--destination=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
+		--context=.
+	/kaniko/executor \
+		--build-arg=BASE=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
+		--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
+		--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
+		--build-arg=FVP_PLUGIN_DIR=${FVP_PLUGIN_DIR} \
+		--dockerfile=Dockerfile.fvp \
+		--destination=${REGISTRY}/base-full:${VERSION}-${ARCH} \
+		--context=.
+else
+	echo "Driver ${DRIVER} not supported"
+	exit 1
 fi
