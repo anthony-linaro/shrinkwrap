e77af58c8793ac8791cd84002c379a5543cd5a0d
Ryan Roberts <ryan.roberts@arm.com>
2024-06-14 15:19:35 +0100

    ci: cache base image in gitlab to avoid rate limiting
    docker.io imposes rate limits on pulling images, so let's cache the base
    image in the gitlab registry to avoid this problem.
    
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

--- docker/build.sh
+++ docker/build.sh
@@ -164,7 +164,7 @@ fi
 # Build the images.
 if [ "${DRIVER}" = "docker" ]; then
 	docker build \
-		--build-arg=BASE=docker.io/library/debian:bookworm-slim \
+		--build-arg=BASE=registry.gitlab.arm.com/tooling/shrinkwrap/bookworm-slim \
 		--build-arg=TCH_PKG_NAME_AARCH64=${TCH_PKG_NAME_AARCH64} \
 		--build-arg=TCH_PATH_AARCH64=${TCH_PATH_AARCH64} \
 		--file=Dockerfile.slim \
@@ -208,7 +208,7 @@ elif [ "${DRIVER}" = "kaniko" ]; then
 	/kaniko/executor \
 		--cleanup \
 		--cache=true --cache-copy-layers --cache-run-layers \
-		--build-arg=BASE=docker.io/library/debian:bookworm-slim \
+		--build-arg=BASE=registry.gitlab.arm.com/tooling/shrinkwrap/bookworm-slim \
 		--build-arg=TCH_PKG_NAME_AARCH64=${TCH_PKG_NAME_AARCH64} \
 		--build-arg=TCH_PATH_AARCH64=${TCH_PATH_AARCH64} \
 		--dockerfile=Dockerfile.slim \
