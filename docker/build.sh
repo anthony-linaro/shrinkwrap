76cca55d562f3dba1d31918723976bdc68e0e4ac
Ryan Roberts <ryan.roberts@arm.com>
2025-08-01 15:51:17 +0100

    Revert "docker: include entire repository in build context"
    This reverts commit 40b7d400c22a351caf5c66e3ffaf74b80de52798.
    
    The series causes a bug where the phython environment is not properly
    activated inside the container and as a result commands fail to execute
    properly.
    
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

--- docker/build.sh
+++ docker/build.sh
@@ -137,15 +137,15 @@ wget_or_cache()
 	fi
 }
 
-# Do everything relative to the project workspace.
+# Do everything relative to this script's directory.
 ROOT=$( dirname $( readlink -f "$0" ) )
-cd ${ROOT}/..
+cd ${ROOT}
 
 # Grab the pre-built packages.
-mkdir -p docker/assets
-wget_or_cache docker/assets/${TCH_PKG_NAME_AARCH64} ${TCH_PKG_URL_AARCH64}/${TCH_PKG_NAME_AARCH64}
-wget_or_cache docker/assets/${TCH_PKG_NAME_AARCH32} ${TCH_PKG_URL_AARCH32}/${TCH_PKG_NAME_AARCH32}
-wget_or_cache docker/assets/${FVP_PKG_NAME} ${FVP_PKG_URL}/${FVP_PKG_NAME}
+mkdir -p assets
+wget_or_cache assets/${TCH_PKG_NAME_AARCH64} ${TCH_PKG_URL_AARCH64}/${TCH_PKG_NAME_AARCH64}
+wget_or_cache assets/${TCH_PKG_NAME_AARCH32} ${TCH_PKG_URL_AARCH32}/${TCH_PKG_NAME_AARCH32}
+wget_or_cache assets/${FVP_PKG_NAME} ${FVP_PKG_URL}/${FVP_PKG_NAME}
 
 # Short circuit building the images if requested.
 if [ "${VERSION}" = "none" ]; then
@@ -158,28 +158,28 @@ if [ "${DRIVER}" = "docker" ]; then
 		--build-arg=BASE=registry.gitlab.arm.com/tooling/shrinkwrap/bookworm-slim \
 		--build-arg=TCH_PKG_NAME_AARCH64=${TCH_PKG_NAME_AARCH64} \
 		--build-arg=TCH_PATH_AARCH64=${TCH_PATH_AARCH64} \
-		--file=docker/Dockerfile.slim \
+		--file=Dockerfile.slim \
 		--tag=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
 		.
 	docker build \
 		--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
 		--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
 		--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
-		--file=docker/Dockerfile.fvp \
+		--file=Dockerfile.fvp \
 		--tag=${REGISTRY}/base-slim:${VERSION}-${ARCH} \
 		.
 	docker build \
 		--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
 		--build-arg=TCH_PKG_NAME_AARCH32=${TCH_PKG_NAME_AARCH32} \
 		--build-arg=TCH_PATH_AARCH32=${TCH_PATH_AARCH32} \
-		--file=docker/Dockerfile.full \
+		--file=Dockerfile.full \
 		--tag=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
 		.
 	docker build \
 		--build-arg=BASE=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
 		--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
 		--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
-		--file=docker/Dockerfile.fvp \
+		--file=Dockerfile.fvp \
 		--tag=${REGISTRY}/base-full:${VERSION}-${ARCH} \
 		.
 
@@ -198,7 +198,7 @@ elif [ "${DRIVER}" = "kaniko" ]; then
 		--build-arg=BASE=registry.gitlab.arm.com/tooling/shrinkwrap/bookworm-slim \
 		--build-arg=TCH_PKG_NAME_AARCH64=${TCH_PKG_NAME_AARCH64} \
 		--build-arg=TCH_PATH_AARCH64=${TCH_PATH_AARCH64} \
-		--dockerfile=docker/Dockerfile.slim \
+		--dockerfile=Dockerfile.slim \
 		--destination=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
 		--context=.
 	/kaniko/executor \
@@ -207,7 +207,7 @@ elif [ "${DRIVER}" = "kaniko" ]; then
 		--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
 		--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
 		--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
-		--dockerfile=docker/Dockerfile.fvp \
+		--dockerfile=Dockerfile.fvp \
 		--destination=${REGISTRY}/base-slim:${VERSION}-${ARCH} \
 		--context=.
 	/kaniko/executor \
@@ -216,7 +216,7 @@ elif [ "${DRIVER}" = "kaniko" ]; then
 		--build-arg=BASE=${REGISTRY}/base-slim-nofvp:${VERSION}-${ARCH} \
 		--build-arg=TCH_PKG_NAME_AARCH32=${TCH_PKG_NAME_AARCH32} \
 		--build-arg=TCH_PATH_AARCH32=${TCH_PATH_AARCH32} \
-		--dockerfile=docker/Dockerfile.full \
+		--dockerfile=Dockerfile.full \
 		--destination=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
 		--context=.
 	/kaniko/executor \
@@ -225,7 +225,7 @@ elif [ "${DRIVER}" = "kaniko" ]; then
 		--build-arg=BASE=${REGISTRY}/base-full-nofvp:${VERSION}-${ARCH} \
 		--build-arg=FVP_PKG_NAME=${FVP_PKG_NAME} \
 		--build-arg=FVP_MODEL_DIR=${FVP_MODEL_DIR} \
-		--dockerfile=docker/Dockerfile.fvp \
+		--dockerfile=Dockerfile.fvp \
 		--destination=${REGISTRY}/base-full:${VERSION}-${ARCH} \
 		--context=.
 else
