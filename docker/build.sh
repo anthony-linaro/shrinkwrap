be0e6d63436df1cfa9cff9a52d7d14cac2466a2c
Ryan Roberts <ryan.roberts@arm.com>
2025-10-20 17:04:26 +0100

    docker: Update FVP to 11.29_42
    Update the FVP version packaged inside the shrinkwrap container images
    to 11.29_42.
    
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

--- docker/build.sh
+++ docker/build.sh
@@ -105,8 +105,8 @@ if [ "${ARCH}" = "x86_64" ]; then
 	TCH_PKG_URL_AARCH32=https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel
 	TCH_PKG_NAME_AARCH32=arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz
 	TCH_PATH_AARCH32=arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi/bin
-	FVP_PKG_URL=https://developer.arm.com/-/cdn-downloads/permalink/FVPs-Architecture/FM-11.29
-	FVP_PKG_NAME=FVP_Base_RevC-2xAEMvA_11.29_27_Linux64.tgz
+	FVP_PKG_URL=https://developer.arm.com/-/cdn-downloads/permalink/FVPs-Architecture/FM-11.29-42
+	FVP_PKG_NAME=FVP_Base_RevC-2xAEMvA_11.29_42_Linux64.tgz
 	FVP_MODEL_DIR=Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3
 # ARCH is "aarch64" on Ubuntu, or "arm64" on Mac OS
 elif [ "${ARCH}" = "aarch64" ] || [ "${ARCH}" = "arm64" ]; then
@@ -116,8 +116,8 @@ elif [ "${ARCH}" = "aarch64" ] || [ "${ARCH}" = "arm64" ]; then
 	TCH_PKG_URL_AARCH32=https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel
 	TCH_PKG_NAME_AARCH32=arm-gnu-toolchain-13.2.rel1-aarch64-arm-none-eabi.tar.xz
 	TCH_PATH_AARCH32=arm-gnu-toolchain-13.2.Rel1-aarch64-arm-none-eabi/bin
-	FVP_PKG_URL=https://developer.arm.com/-/cdn-downloads/permalink/FVPs-Architecture/FM-11.29
-	FVP_PKG_NAME=FVP_Base_RevC-2xAEMvA_11.29_27_Linux64_armv8l.tgz
+	FVP_PKG_URL=https://developer.arm.com/-/cdn-downloads/permalink/FVPs-Architecture/FM-11.29-42
+	FVP_PKG_NAME=FVP_Base_RevC-2xAEMvA_11.29_42_Linux64_armv8l.tgz
 	FVP_MODEL_DIR=Base_RevC_AEMvA_pkg/models/Linux64_armv8l_GCC-9.3
 else
 	echo "Host architecture ${ARCH} not supported"
