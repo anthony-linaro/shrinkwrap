anthony-linaro/shrinkwrap

commit 22e540d9ad43e00b6d433c8b2f394a478d5d9560

submodules: Support for --reference when cloning submodules
This update extends project cache directory support to submodules. All
submodules can be updated in the project cache by running the following
command from the top-level cache directory:

git submodule update --init --checkout --recursive --force
or by cloning with git clone --recurse-submodules

Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
Signed-off-by: Aneesh Kumar K.V (Arm) <aneesh.kumar@kernel.org>

shrinkwrap/utils/config.py
@@ -860,6 +860,40 @@ def script_preamble(echo):
 	else:
 		pre.append(f'# Exit on error.')
 		pre.append(f'set -e')
+
+	gitargs = '"--quiet "' if not echo else '""'
+	pre.append(f'gitargs={gitargs}')
+	pre.append_multiline('''
+	# Function to update submodules without recursion
+	update_submodules() {
+		local repo_path="$1"
+		local reference_path="$2"
+
+		local cwd=$(pwd)
+		cd $repo_path
+		# Check if .gitmodules file exists
+		local gitmodules_file=".gitmodules"
+		if [ -f "$gitmodules_file" ]; then
+			# Extract submodule paths from .gitmodules
+			git config --file "$gitmodules_file" --get-regexp path | while read -r path_key submodule_path; do
+				local git_submodule_reference=""
+				local submodule_reference="$reference_path/$submodule_path"
+
+				# Check if the submodule exists in the project cache
+				if [[ -n "$reference_path" && -e "$submodule_reference/.git" ]]; then
+					git_submodule_reference="--reference $submodule_reference"
+				fi
+
+				if [ -d "$submodule_path" ]; then
+					# Manually update nested submodules
+					git submodule $gitargs update --init --checkout --force $git_submodule_reference $submodule_path
+					# Recursively process the submodule
+					update_submodules "$submodule_path" "$submodule_reference"
+				fi
+			done
+		fi
+		cd $cwd
+	}''')
 	return pre.commands(False)
 
 
@@ -999,7 +1033,8 @@ def build_graph(configs, echo, nosync, force_sync):
 							git clone {gitargs}{git_local_reference}{gitremote} {gitlocal}
 							pushd {gitlocal}
 							git checkout {gitargs}--force {gitrev}
-							git submodule {gitargs}update --init --checkout --recursive --force
+							# run with --reference
+							update_submodules "$(pwd)" "{git_project_cache}"
 							popd
 							rm {sync}
 						else

commit eb5857634b59c9ff5d6a622cbd30eee2af08006f

run: Don't show update_submodules() bash function
Commit 22e540d ("submodules: Support for --reference when cloning
submodules") added a bash update_submodules() script to
script_preamble(). The script is used when building to checkout the
repository for a component. But script_preamble() provides the preamble
for all commands (build, clean, run). It is not needed for clean and
run, so let's hide it from those.

This tidies up the --dry-run output.

Fixes: 22e540d ("submodules: Support for --reference when cloning submodules")
Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

shrinkwrap/utils/config.py
@@ -872,12 +872,15 @@ def script_preamble(echo):
 	else:
 		pre.append(f'# Exit on error.')
 		pre.append(f'set -e')
+	return pre.commands(False)
 
-	gitargs = '"--quiet "' if not echo else '""'
-	pre.append(f'gitargs={gitargs}')
-	pre.append_multiline('''
+
+def script_build_preamble(echo):
+	pre = Script(None)
+	gitargs = '' if echo else '--quiet '
+	pre.append_multiline(f'''
 	# Function to update submodules without recursion
-	update_submodules() {
+	update_submodules() {{
 		local repo_path="$1"
 		local reference_path="$2"
 
@@ -898,14 +901,14 @@ def script_preamble(echo):
 
 				if [ -d "$submodule_path" ]; then
 					# Manually update nested submodules
-					git submodule $gitargs update --init --checkout --force $git_submodule_reference $submodule_path
+					git submodule {gitargs} update --init --checkout --force $git_submodule_reference $submodule_path
 					# Recursively process the submodule
 					update_submodules "$submodule_path" "$submodule_reference"
 				fi
 			done
 		fi
 		cd $cwd
-	}''')
+	}}''')
 	return pre.commands(False)
 
 
@@ -919,6 +922,7 @@ def build_graph(configs, echo, nosync, force_sync):
 	gitargs = '' if echo else '--quiet '
 
 	pre = script_preamble(echo)
+	pre += script_build_preamble(echo)
 
 	gl1 = Script('Removing old package', preamble=pre)
 	gl1.append(f'# Remove old package.')