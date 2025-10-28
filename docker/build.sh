anthony-linaro/shrinkwrap

commit ec08849bf7852f893bfb5e67ab3175444533efe1

ci: Add gitlab pipeline definition
The pipeline can automatically build the docker images and run the self
tests in the resulting base-full image. If the builds and tests succeed,
and if the target branch is "main", the images are published to docker
hub under the "latest" tag. Works for both x86_64 and arm64.

Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

.gitlab-ci.yml
@@ -1,0 +1,140 @@
+variables:
+  KUBERNETES_CPU_REQUEST: 32
+  KUBERNETES_MEMORY_REQUEST: 16Gi
+  FORCE_PREP_TEST_ASSETS:
+    value: "false"
+    options:
+      - "true"
+      - "false"
+    description: "If true, forces test assets to be prepped even if cached from a previous run."
+  FORCE_PREP_DOCKER_ASSETS:
+    value: "false"
+    options:
+      - "true"
+      - "false"
+    description: "If true, forces docker assets to be prepped even if cached from a previous run."
+
+stages:
+  - prep
+  - build
+  - test
+  - deploy
+
+prep-docker-assets:
+  stage: prep
+  image: shrinkwraptool/base-slim:latest
+  tags:
+    - amd64
+  cache:
+    key: docker-assets
+    policy: pull-push
+    paths:
+      - docker/assets/
+  script:
+    - |
+      if [ -f ./docker/assets/.cache_exists ] && [ "$FORCE_PREP_DOCKER_ASSETS" == "false" ]; then
+        echo "Cache already exists and prep not forced: skipping."
+      else
+        ./docker/build.sh --version none --arch x86_64
+        ./docker/build.sh --version none --arch aarch64
+        echo > ./docker/assets/.cache_exists
+      fi
+
+build-docker-image:
+  stage: build
+  image:
+    name: gcr.io/kaniko-project/executor:debug
+    entrypoint: [""]
+  parallel:
+    matrix:
+      - TAG:
+        - amd64
+        - arm64
+  tags:
+    - ${TAG}
+  cache:
+    key: docker-assets
+    policy: pull
+    paths:
+      - docker/assets/
+  script:
+    - mkdir -p /kaniko/.docker
+    - echo "{\"auths\":{\"${CI_REGISTRY_IMAGE}\":{\"auth\":\"$(printf "%s:%s" "${CI_REGISTRY_USER}" "${CI_REGISTRY_PASSWORD}" | base64 | tr -d '\n')\"}}}" > /kaniko/.docker/config.json
+    - ./docker/build.sh --driver kaniko --registry ${CI_REGISTRY_IMAGE} --version ${CI_PIPELINE_IID}
+
+build-docker-manifest:
+  stage: build
+  image: shrinkwraptool/base-slim:latest
+  tags:
+    - amd64
+  needs:
+    - build-docker-image
+  script:
+    - mkdir bin
+    - export PATH=${PWD}/bin:${PATH}
+    - curl -LsS https://github.com/estesp/manifest-tool/releases/download/v2.0.6/binaries-manifest-tool-2.0.6.tar.gz | tar -xz -C bin
+    - ./docker/publish.sh --driver manifest-tool --user ${CI_REGISTRY_USER} --password ${CI_REGISTRY_PASSWORD} --registry ${CI_REGISTRY_IMAGE} --version ${CI_PIPELINE_IID}
+
+prep-test-assets:
+  stage: prep
+  image: shrinkwraptool/base-slim:latest
+  tags:
+    - amd64
+  cache:
+    key: test-assets
+    policy: pull-push
+    paths:
+      - test/assets/
+  script:
+    - |
+      if [ -f ./test/assets/.cache_exists ] && [ "$FORCE_PREP_TEST_ASSETS" == "false" ]; then
+        echo "Cache already exists and prep not forced: skipping."
+      else
+        ./test/genassets.sh
+        echo > ./test/assets/.cache_exists
+      fi
+
+test-self:
+  stage: test
+  image: registry.gitlab.arm.com/tooling/shrinkwrap/base-full:${CI_PIPELINE_IID}
+  parallel:
+    matrix:
+      - TAG:
+        - amd64
+        - arm64
+  tags:
+    - ${TAG}
+  cache:
+    key: test-assets
+    policy: pull
+    paths:
+      - test/assets/
+  script:
+    - export PATH=$PWD/shrinkwrap:$PATH
+    - export SHRINKWRAP_BUILD=$PWD/shrinkwrap_workspace
+    - export SHRINKWRAP_PACKAGE=$PWD/shrinkwrap_workspace/package
+    - ./test/test.py --runtime null --smoke-test --fvpjobs 4 --junit selftest-${TAG}.xml
+  artifacts:
+    name: selftest-${TAG}
+    paths:
+      - selftest-${TAG}.xml
+    reports:
+      junit: selftest-${TAG}.xml
+
+deploy-docker-image:
+  stage: deploy
+  image: shrinkwraptool/base-slim:latest
+  tags:
+    - amd64
+  rules:
+    - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH
+  script:
+    - mkdir bin
+    - export PATH=${PWD}/bin:${PATH}
+    - curl -LsS https://github.com/regclient/regclient/releases/latest/download/regctl-linux-amd64 > bin/regctl
+    - chmod 755 bin/regctl
+    - regctl registry login -u ${DOCKERHUB_USER} -p ${DOCKERHUB_PASSWORD} docker.io
+    - regctl image copy registry.gitlab.arm.com/tooling/shrinkwrap/base-slim-nofvp:${CI_PIPELINE_IID} docker.io/shrinkwraptool/base-slim-nofvp:latest
+    - regctl image copy registry.gitlab.arm.com/tooling/shrinkwrap/base-slim:${CI_PIPELINE_IID} docker.io/shrinkwraptool/base-slim:latest
+    - regctl image copy registry.gitlab.arm.com/tooling/shrinkwrap/base-full-nofvp:${CI_PIPELINE_IID} docker.io/shrinkwraptool/base-full-nofvp:latest
+    - regctl image copy registry.gitlab.arm.com/tooling/shrinkwrap/base-full:${CI_PIPELINE_IID} docker.io/shrinkwraptool/base-full:latest

commit bd7e66203f78f69086732fe4c364f6ac4e10ab0c

ci: Ensure caches get correctly invalidated
Previously if .cache-exists was found in the docker-assets cache, no
attempt was made to download the required files into the cache. This is
"safe" since when building the container, if the required files are not
present, they are downloaded. But this is problematic for 2 reasons; it
is slow. The whole point of the cache is to avoid the download. But more
importantly, the kaniko image that we are stuck using to create the
image provides a buggy wget so it fails.

build.sh, when invoked with --version none, will already avoid download
if the correct files are present, so let's just always call that to
(potentially) repopulate the cache. Then kaniko's wget never needs to be
used.

While we are at it, let's rework to always delete anything in the cache
if requested by the user.

Fixes: ec08849 ("ci: Add gitlab pipeline definition")
Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

.gitlab-ci.yml
@@ -7,18 +7,18 @@ variables:
       - "true"
       - "false"
     description: "If true, runs a subset of tests to speed up pipeline execution time."
-  FORCE_PREP_TEST_ASSETS:
+  INVALIDATE_TEST_ASSETS:
     value: "false"
     options:
       - "true"
       - "false"
-    description: "If true, forces test assets to be prepped even if cached from a previous run."
-  FORCE_PREP_DOCKER_ASSETS:
+    description: "If true, forces any cached test assets to be invalidated prior to repopulation."
+  INVALIDATE_DOCKER_ASSETS:
     value: "false"
     options:
       - "true"
       - "false"
-    description: "If true, forces docker assets to be prepped even if cached from a previous run."
+    description: "If true, forces any cached docker assets to be invalidated prior to repopulation."
 
 workflow:
   rules:
@@ -46,13 +46,14 @@ prep-docker-assets:
       - docker/assets/
   script:
     - |
-      if [ -f ./docker/assets/.cache_exists ] && [ "$FORCE_PREP_DOCKER_ASSETS" == "false" ]; then
-        echo "Cache already exists and prep not forced: skipping."
-      else
-        ./docker/build.sh --version none --arch x86_64
-        ./docker/build.sh --version none --arch aarch64
-        echo > ./docker/assets/.cache_exists
+      if [ "$INVALIDATE_DOCKER_ASSETS" == "true" ]; then
+        echo "Invalidating docker assets cache..."
+        rm -rf docker/assets/*
       fi
+      echo "Populating docker assets cache for x86_64..."
+      ./docker/build.sh --version none --arch x86_64
+      echo "Populating docker assets cache for aarch64..."
+      ./docker/build.sh --version none --arch aarch64
 
 build-docker-image:
   stage: build
@@ -132,9 +133,12 @@ prep-test-assets:
       - test/assets/
   script:
     - |
-      if [ -f ./test/assets/.cache_exists ] && [ "$FORCE_PREP_TEST_ASSETS" == "false" ]; then
-        echo "Cache already exists and prep not forced: skipping."
-      else
+      if [ "$INVALIDATE_TEST_ASSETS" == "true" ]; then
+        echo "Invalidating test assets cache..."
+        rm -rf test/assets/*
+      fi
+      if [ ! -f ./test/assets/.cache_exists ]; then
+        echo "Generating test assets..."
         ./test/genassets.sh -R null
         echo > ./test/assets/.cache_exists
       fi

docker/build.sh
@@ -133,7 +133,10 @@ wget_or_cache()
 	URL=$2
 
 	if [ ! -f ${FILE} ]; then
+		echo "Downloading ${FILE}"
 		wget -q -O ${FILE} ${URL}
+	else
+		echo "Already have ${FILE}"
 	fi
 }
 