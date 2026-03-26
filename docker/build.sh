c03e9b53f4083ede0d7fbcb66b56b6fe617ef2c3
Ryan Roberts <ryan.roberts@arm.com>
2026-03-26 09:23:35 +0000

    docker: Update FVP to 11.31_28
    Update the FVP version packaged inside the shrinkwrap container images
    to 11.31_28.
    
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

--- docker/build.sh
+++ docker/build.sh
@@ -105,9 +105,9 @@ if [ "${ARCH}" = "x86_64" ]; then
 	TCH_PKG_URL_AARCH32=https://developer.arm.com/-/media/Files/downloads/gnu/14.3.rel1/binrel
 	TCH_PKG_NAME_AARCH32=arm-gnu-toolchain-14.3.rel1-x86_64-arm-none-eabi.tar.xz
 	TCH_PATH_AARCH32=arm-gnu-toolchain-14.3.rel1-x86_64-arm-none-eabi/bin
-	FVP_PKG_URL=https://developer.arm.com/-/cdn-downloads/permalink/FVPs-Architecture/FM-11.30
-	FVP_PKG_NAME=FVP_Base_RevC-2xAEMvA_11.30_27_Linux64.tgz
-	FVP_MODEL_DIR=Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3
+	FVP_PKG_URL=https://developer.arm.com/-/cdn-downloads/permalink/FVPs-Architecture/FM-11.31
+	FVP_PKG_NAME=FVP_Base_RevC_AEMvA_11.31_28_Linux_x86.tar.gz
+	FVP_MODEL_DIR=FVP_Base_RevC_AEMvA_11.31_28/bin
 # ARCH is "aarch64" on Ubuntu, or "arm64" on Mac OS
 elif [ "${ARCH}" = "aarch64" ] || [ "${ARCH}" = "arm64" ]; then
 	TCH_PKG_URL_AARCH64=https://developer.arm.com/-/media/Files/downloads/gnu/14.3.rel1/binrel
@@ -116,9 +116,9 @@ elif [ "${ARCH}" = "aarch64" ] || [ "${ARCH}" = "arm64" ]; then
 	TCH_PKG_URL_AARCH32=https://developer.arm.com/-/media/Files/downloads/gnu/14.3.rel1/binrel
 	TCH_PKG_NAME_AARCH32=arm-gnu-toolchain-14.3.rel1-aarch64-arm-none-eabi.tar.xz
 	TCH_PATH_AARCH32=arm-gnu-toolchain-14.3.rel1-aarch64-arm-none-eabi/bin
-	FVP_PKG_URL=https://developer.arm.com/-/cdn-downloads/permalink/FVPs-Architecture/FM-11.30
-	FVP_PKG_NAME=FVP_Base_RevC-2xAEMvA_11.30_27_Linux64_armv8l.tgz
-	FVP_MODEL_DIR=Base_RevC_AEMvA_pkg/models/Linux64_armv8l_GCC-9.3
+	FVP_PKG_URL=https://developer.arm.com/-/cdn-downloads/permalink/FVPs-Architecture/FM-11.31
+	FVP_PKG_NAME=FVP_Base_RevC_AEMvA_11.31_28_Linux_armv8.tar.gz
+	FVP_MODEL_DIR=FVP_Base_RevC_AEMvA_11.31_28/bin
 else
 	echo "Host architecture ${ARCH} not supported"
 	usage
